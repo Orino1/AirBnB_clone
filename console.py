d7309fc5be119e75b85fb2c37805a8379a054dd1
walid <jabiriwalid555@.com>
2023-08-13 23:30:05 +0000

    edit console.py

--- console.py
+++ console.py
@@ -8,22 +8,26 @@ class HBNBCommand(cmd.Cmd):
     A basic command interpreter class.
     """
     prompt = "(hbnb) "
+
     def do_quit(self, arg):
         """
         Quit command to exit the program
         """
         return True
+
     def do_EOF(self, arg):
         """
         Exit the program (Ctrl-D).
         """
         print("")
         return True
+
     def emptyline(self):
         """
         Do nothing when an empty line is entered.
         """
         pass
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
 
