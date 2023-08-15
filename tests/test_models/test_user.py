634adce38a82f0d3c64f79cfc39f1dfa65449b61
Orino1 <orinomohamed@gmail.com>
2023-08-15 03:24:55 +0100

    Update: TTesting a simple test case

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,34 +1,32 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+"""
+Test model
+"""
+import unittest
 from models.user import User
 
 
-class test_User(test_basemodel):
-    """ """
-
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "User"
-        self.value = User
+class Test_User(unittest.TestCase):
+    """
+    Test_User class: Test cases for the User class.
+    """
 
     def test_first_name(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.first_name), str)
+        user_obj = User()
+        self.assertEqual(type(user_obj.first_name), str)
 
     def test_last_name(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.last_name), str)
+        user_obj = User()
+        self.assertEqual(type(user_obj.last_name), str)
 
     def test_email(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.email), str)
+        user_obj = User()
+        self.assertEqual(type(user_obj.email), str)
 
     def test_password(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.password), str)
+        user_obj = User()
+        self.assertEqual(type(user_obj.password), str)
