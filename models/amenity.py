c6dd06149c179941c983a7c7bcf4a88261a47a82
Orino1 <orinomohamed@gmail.com>
2023-08-15 03:41:07 +0100

    Adding amenity.py File

---
+++ models/amenity.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A basic Amenity model.
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Amenity class inherits from BaseModel.
+    """
+
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize Amenity class
+        """
+        super().__init__(*args, **kwargs)
