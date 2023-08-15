cdfba5adb72d5d315c55ca36d8ffabd5dda9824f
Orino1 <orinomohamed@gmail.com>
2023-08-15 03:48:36 +0100

    Update: Adding support for additional classes

--- console.py
+++ console.py
@@ -1,19 +1,35 @@
 #!/usr/bin/python3
 """
 A basic command interpreter model.
+TODO: this is not a basic model any more
 """
+# TODO: the only red checker is about non-interactive mode
 import cmd
 from models.base_model import BaseModel
 from models import storage
 from models.user import User
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.state import State
 
 class HBNBCommand(cmd.Cmd):
     """
     A basic command interpreter class.
+    TODO: Also not basic anymore
     """
 
     prompt = "(hbnb) "
-    classes = {"BaseModel": BaseModel, "User": User }
+    classes = {
+        "BaseModel": BaseModel,
+        "User": User,
+        "Place": Place,
+        "State": State,
+        "City": City,
+        "Amenity": Amenity,
+        "Review": Review
+    }
 
     def do_quit(self, arg):
         """
