81a07b1619be701848baa252b6d71887522d7d45
Orino1 <orinomohamed@gmail.com>
2023-08-15 03:43:26 +0100

    Adding test_review.py File

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Test model
+"""
+import unittest
+from models.review import Review
+
+
+class TestReview(unittest.TestCase):
+    """
+    Test_User class: Test cases for the City class.
+    """
+
+    def test_user_id(self):
+        """ """
+        user_obj = Review()
+        self.assertEqual(type(user_obj.user_id), str)
+    
+    def test_place_id(self):
+        """ """
+        user_obj = Review()
+        self.assertEqual(type(user_obj.place_id), str)
+
+    def test_text(self):
+        """ """
+        user_obj = Review()
+        self.assertEqual(type(user_obj.text), str)
