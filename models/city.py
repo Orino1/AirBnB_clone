0fb7e325a04d1f0ff3f50d9c5239c9403810bcde
Orino1 <orinomohamed@gmail.com>
2023-08-15 03:41:15 +0100

    Adding city.py File

---
+++ models/city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+A basic City model.
+"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+    City class inherits from BaseModel.
+    """
+
+    state_id = ""
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize City class
+        """
+        super().__init__(*args, **kwargs)
