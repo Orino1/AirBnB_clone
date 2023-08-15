7b4b7ae80d6e9df18b8bfc7bc980e9b8229f79a4
Orino1 <orinomohamed@gmail.com>
2023-08-15 03:41:47 +0100

    Adding review.py File

---
+++ models/review.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+A basic Review model.
+"""
+
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """
+    Review class that inherits from BaseModel.
+    """
+
+    place_id = ""
+    user_id = ""
+    text = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize Review class
+        """
+        super().__init__(*args, **kwargs)
