4216bd89740147990b2c8ed504b8858bc2a6d152
Orino1 <orinomohamed@gmail.com>
2023-08-15 03:49:40 +0100

    Update: Adding support for additional classes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,6 +9,11 @@ import os
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.state import State
 
 
 class FileStorage:
@@ -21,7 +26,15 @@ class FileStorage:
 
     __file_path = "file.json"
     __objects = {}
-    classes = {"BaseModel": BaseModel, "User": User }
+    classes = {
+        "BaseModel": BaseModel,
+        "User": User,
+        "Place": Place,
+        "State": State,
+        "City": City,
+        "Amenity": Amenity,
+        "Review": Review
+    }
     def all(self):
         """
         Returns the dictionary of all objects currently stored.
