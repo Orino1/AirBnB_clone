3cab6dc6edf176bed57cd454fbec2c56f98bd2ad
Orino1 <orinomohamed@gmail.com>
2023-08-15 03:42:25 +0100

    Adding test_city.py File

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Test model
+"""
+import unittest
+from models.city import City
+
+
+class TestCity(unittest.TestCase):
+    """
+    Test_User class: Test cases for the City class.
+    """
+
+    def test_name(self):
+        """ """
+        user_obj = City()
+        self.assertEqual(type(user_obj.name), str)
+    
+    def test_state_id(self):
+        """ """
+        user_obj = City()
+        self.assertEqual(type(user_obj.state_id), str)
